c8269c24efc5806450d4412edd48d0b3006f085d
Elías Zelada <elixs.777@gmail.com>
2024-03-18 00:59:12 -0300

    removing player

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,11 +1,1 @@
 extends Node2D
-
-@export var player_scene: PackedScene
-@onready var players: Node2D = $Players
-
-
-func _ready() -> void:
-	for player_data in Game.players:
-		var player = player_scene.instantiate()
-		players.add_child(player)
-		player.setup(player_data)
