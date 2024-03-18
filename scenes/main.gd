b03076e378a0f5c99ca90e5822a6f4f4d8692cba
Elías Zelada <elixs.777@gmail.com>
2024-03-18 18:25:00 -0300

    adding players

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,1 +1,15 @@
 extends Node2D
+
+
+#var player_scene = preload("res://scenes/player.tscn")
+@export var player_scene: PackedScene
+@onready var players: Node2D = $Players
+
+
+func _ready() -> void:
+	for player_data in Game.players:
+		var player = player_scene.instantiate()
+		
+		players.add_child(player)
+		player.setup(player_data)
+		
