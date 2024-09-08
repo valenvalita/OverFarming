1444c42cc756ee0990a2d79e200e7fcfea4a3aed
valenvalita <91163931+valenvalita@users.noreply.github.com>
2024-09-08 15:32:01 -0300

    feat(colisiones): se agrega colisiones para edificios

---
+++ scenes/area_transparent.gd
@@ -0,0 +1,18 @@
+extends Area2D
+@onready var supermarket: Node2D = $".."
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		supermarket.modulate = Color(1, 1, 1, 0.5)
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		supermarket.modulate = Color(1, 1, 1, 1)
