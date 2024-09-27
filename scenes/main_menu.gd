2c98c4777afccecf560b361c1823d0ff524938b2
Javier Atenas <xavieratenas@gmail.com>
2024-09-27 13:24:14 -0300

    feat!: Se crea el menu principal

---
+++ scenes/main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_volumen_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_resolution_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
