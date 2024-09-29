802e2ac28240ddc7738f0fd9403ec0d38ba4bd62
Javier Atenas <xavieratenas@gmail.com>
2024-09-29 03:52:05 -0300

    feat!: Se crea menu de pausa

---
+++ scripts/pause_menu.gd
@@ -0,0 +1,31 @@
+extends Control
+
+
+func resume():
+	get_tree().paused = false
+	$AnimationPlayer.play_backwards("blur")
+
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+
+func TestEsc():
+	if Input.is_action_just_pressed("esc") and !get_tree().paused:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused:
+		resume()
+
+func _process(delta):
+	TestEsc()
+
+
+func _on_reanude_pressed() -> void:
+	resume()
+	
+	
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/ui/main_menu.tscn")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
