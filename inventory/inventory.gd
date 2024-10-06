5190790aba83e6dffb782dfb4280228a505b1394
valenvalita <91163931+valenvalita@users.noreply.github.com>
2024-10-05 22:50:37 -0300

    feat(inventory): se agrega maximo de items en inventario

--- inventory/inventory.gd
+++ inventory/inventory.gd
@@ -4,14 +4,22 @@ class_name Inv
 
 signal update
 @export var slots: Array[InvSlot]
+var max_items_per_slot_dft = 10
 
 func insert(item: InvItem):
+	var max_items_per_slot = item.max_items_per_slot if item.max_items_per_slot > 0 else max_items_per_slot_dft
 	var itemslots = slots.filter(func(slot): return slot.item==item)
 	if !itemslots.is_empty():
-		itemslots[0].amount += 1
+		if itemslots[0].amount < max_items_per_slot:
+			itemslots[0].amount += 1
+		else:
+			print("Ya has alcanzado el máximo de almacenamiento")
 	else:
 		var emptyslots = slots.filter(func(slot): return slot.item == null)
 		if !emptyslots.is_empty():
-			emptyslots[0].item = item
-			emptyslots[0].amount = 1
+			if emptyslots[0].amount < max_items_per_slot:
+				emptyslots[0].item = item
+				emptyslots[0].amount = 1
+			else:
+				print("Ya has alcanzado el máximo de almacenamiento")
 	update.emit()
