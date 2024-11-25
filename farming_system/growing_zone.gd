71e781a297554f3ae743711b3d9073a981cb1911
Javier Atenas <xavieratenas@gmail.com>
2024-11-25 19:33:45 -0300

    fix(growing zone): arreglo del bug

--- farming_system/growing_zone.gd
+++ farming_system/growing_zone.gd
@@ -49,7 +49,7 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 
 ## LÓGICA PARA CAVAR TIERRA ##
 func dig_soil():
-	Debug.log("Se cava hoyo")
+	#Debug.log("Se cava hoyo")
 	_server_dig_soil.rpc_id(1)
 
 @rpc("any_peer","call_local","reliable")
@@ -63,63 +63,38 @@ func _sync_dig_soil(new_state):
 	
 ## LÓGICA PARA PLANTAR SEMILLAS ##
 func plant_seed(player):
-	print("Se intenta sembrar")
+	Debug.log("Se intenta sembrar")
 	if player_has_seed(player):
 		var seed_item = player.get_seed()
 		player.remove_item_cnt(seed_item, 1)
-		print("Semilla plantada")
-		
-		if is_multiplayer_authority():
-			rpc_id(0, "_server_plant_seed")
-		else:
-			rpc("_request_plant_seed")	
+		Debug.log("Semilla plantada")
+		_server_plant_seed.rpc_id(1)
 	else:
-		print("Jugador no tiene semillas")
+		Debug.log("Jugador no tiene semillas")
 		
 func player_has_seed(player):
 	return player.has_seed()		
-	
-@rpc("any_peer")  # Permitir que cualquier cliente llame a esta función
-func _request_plant_seed():
-	# Esta función es llamada por el cliente y la ejecuta el servidor
-	if is_multiplayer_authority():  # Verifica si este nodo es el servidor
-		_server_plant_seed()
 
-@rpc("authority")  # Solo el servidor tiene autoridad para ejecutar esta función
+@rpc("any_peer","call_local","reliable")
 func _server_plant_seed():
-	# Aquí el servidor maneja la lógica de plantar la semilla
-	current_state = SoilState.SEEDED_SOIL
-	soil_sprite.frame = 2
-	# Envía la actualización de estado a todos los clientes
-	rpc("_sync_plant_state", current_state)
+	_sync_plant_state.rpc(SoilState.SEEDED_SOIL)
 
-@rpc("any_peer")  # Sincroniza el estado con todos los clientes
+@rpc("any_peer","call_local","reliable")
 func _sync_plant_state(new_state):
 	# Actualiza el estado en los clientes
 	current_state = new_state
-	if current_state == SoilState.SEEDED_SOIL:
-		soil_sprite.frame = 2
+	soil_sprite.frame = 2
 		
 ## LÓGICA PARA REGAR PLANTAS ##	
 func water_soil():
-	print("Se riega")
-	if is_multiplayer_authority():
-		rpc_id(0, "_server_water_soil")
-	else:
-		rpc("_request_water_soil")
+	Debug.log("Se riega")
+	_server_water_soil.rpc_id(1)
 
-@rpc("any_peer")  
-func _request_water_soil():
-	if is_multiplayer_authority(): 
-		_server_water_soil()
-
-@rpc("authority")  
+@rpc("any_peer","call_local","reliable")
 func _server_water_soil():
-	current_state = SoilState.WATERED_SOIL
-	soil_sprite.frame = 3
-	rpc("_sync_water_soil", current_state)
+	_sync_water_soil.rpc(SoilState.WATERED_SOIL)
 
-@rpc("any_peer") 
+@rpc("any_peer","call_local","reliable")
 func _sync_water_soil(new_state):
 	current_state = new_state
 	soil_sprite.frame = 3
@@ -129,35 +104,26 @@ func _on_start_growing_plant_timeout() -> void:
 	grow_plant()
 		
 func grow_plant():
-	if is_multiplayer_authority():
-		_server_grow_plant()
-	else:
-		rpc("_request_grow_plant")
+	_server_grow_plant.rpc_id(1)
 
-@rpc("any_peer")
-func _request_grow_plant():
-	if is_multiplayer_authority():
-		_server_grow_plant()
-
-@rpc("authority")
+@rpc("any_peer","call_local","reliable")
 func _server_grow_plant():
 	plant_growing = true
 	grow_timer.start()
 	animated_plant.play("carrot_growing")
-	rpc("_sync_grow_plant", plant_growing)
+	_sync_grow_plant.rpc(plant_growing)
 
-@rpc("any_peer")
+@rpc("any_peer","call_local","reliable")
 func _sync_grow_plant(is_growing):
 	plant_growing = is_growing
 	animated_plant.play("carrot_growing")
 	grow_timer.start()
 
 func _on_grow_timer_timeout() -> void:
-	if is_multiplayer_authority():
-		_server_grow_step()
-		animated_plant.frame += 1
+	_server_grow_step.rpc_id(1)
+	animated_plant.frame += 1
 
-@rpc("authority")
+@rpc("any_peer","call_local","reliable")
 func _server_grow_step():
 	var carrot_plant = animated_plant
 	if carrot_plant.frame == 0:
@@ -168,30 +134,22 @@ func _server_grow_step():
 		plant_grown = true
 	else:
 		carrot_plant.frame += 1
-	rpc("_sync_grow_step", carrot_plant.frame, current_state, plant_grown)
+	_sync_grow_step.rpc(carrot_plant.frame, current_state, plant_grown)
 
-@rpc("any_peer")
+@rpc("any_peer","call_local","reliable")
 func _sync_grow_step(frame, new_state, is_grown):
 	animated_plant.frame = frame
 	current_state = new_state
 	plant_grown = is_grown
 	if plant_grown:
-		print("La planta ya creció")
+		Debug.log("La planta ya creció")
 		
 ## LÓGICA PARA RECOGER PLANTAS ##
 func pick_plant():
 	print("Se recoge planta")
-	if is_multiplayer_authority():
-		rpc_id(0, "_server_pick_plant")
-	else:
-		rpc("_request_pick_plant")
-		
-@rpc("any_peer")  
-func _request_pick_plant():
-	if is_multiplayer_authority(): 
-		_server_pick_plant()
+	_server_pick_plant.rpc_id(1)
 
-@rpc("authority")
+@rpc("any_peer","call_local","reliable")
 func _server_pick_plant():
 	current_state = SoilState.BARE_SOIL
 	animated_plant.play("default")
@@ -199,10 +157,10 @@ func _server_pick_plant():
 	plant_grown = false
 	rpc("_sync_pick_plant", current_state)
 
-@rpc("any_peer")
+@rpc("any_peer","call_local","reliable")
 func _sync_pick_plant(new_state):
 	current_state = new_state
 	animated_plant.play("default")
 	plant_growing = false
 	plant_grown = false
-	print("Planta recogida")
+	Debug.log("Planta recogida")
