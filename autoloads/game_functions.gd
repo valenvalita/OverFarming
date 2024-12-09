3333f8aab5aa28c09a72afd64f7c90054d1d4000
Rodrigo Manríquez <rodrigo.manriquez.m@ug.uchile.cl>
2024-12-09 18:33:19 -0300

    Se soluciona problema del contador y animaciones
    Ahora las plantas crecen en todos los usuarios y los contadores se actualizan correctamente

--- autoloads/game_functions.gd
+++ autoloads/game_functions.gd
@@ -6,6 +6,10 @@ var current_state = GameState.PLAYING
 var can_pause = true
 var rng = RandomNumberGenerator.new()
 
+# { id: Item }
+var item_data := {}
+var item_data_path := "res://inventory/items"
+
 signal element_updated(element)
 @export var element_index = rng.randi_range(0,3):
 	set(val):
@@ -19,15 +23,19 @@ signal element_updated2(element)
 		element_updated2.emit(element_index2)
 
 signal delivery_updated(new_count)
-@export var n_delivery_carrots = rng.randi_range(1, 20):
+#@export var n_delivery_carrots = rng.randi_range(1, 20):
+@export var n_delivery_carrots = 3:
 	set(value): 
 		n_delivery_carrots = value
+		print("Actualiza valor a: ", n_delivery_carrots)
 		delivery_updated.emit(n_delivery_carrots)
 
 signal delivery_updated2(new_count)
-@export var n_delivery_carrots2 = generate_different_random(n_delivery_carrots, 1, 20):
+#@export var n_delivery_carrots2 = generate_different_random(n_delivery_carrots, 1, 20):
+@export var n_delivery_carrots2 = 3:
 	set(value): 
 		n_delivery_carrots2 = value
+		print("Actualiza valor a: ", n_delivery_carrots2)
 		delivery_updated2.emit(n_delivery_carrots2)
 
 func generate_different_random(num : int, min : int, max : int) -> int :
@@ -43,16 +51,19 @@ func win_screen()-> void:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	load_data()
 
-func update_delivery(cnt_items):
-	update_delivery_server.rpc_id(1, cnt_items)
+func update_delivery(cnt_items, index):
+	update_delivery_server.rpc_id(1, cnt_items, index)
 		
 @rpc("any_peer", "call_local")
-func update_delivery_server(cnt_items):
-	n_delivery_carrots = max(n_delivery_carrots - cnt_items, 0)
-	print(n_delivery_carrots)
-	if n_delivery_carrots==0:
+func update_delivery_server(cnt_items, index):
+	if index == 1:
+		n_delivery_carrots = max(n_delivery_carrots - cnt_items, 0)
+	elif index == 2:
+		n_delivery_carrots2 = max(n_delivery_carrots2 - cnt_items, 0)
+		
+	if n_delivery_carrots==0 and n_delivery_carrots2==0:
 		print("HAS GANADO!")
 		win_screen.rpc()
 	
@@ -72,6 +83,19 @@ func _process(delta: float) -> void:
 		GameState.PLAYING:
 			pass
 			#print("El juego está en progreso")
+func load_data():
+	var dir = DirAccess.open(item_data_path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if not dir.current_is_dir():
+				var id = file_name.split(".")[0]
+				Debug.log("Item added: " + id)
+				item_data[id] = load("%s/%s" % [item_data_path, file_name] )
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
 
 #@rpc("any_peer","call_local","reliable")
 #func win_screen()-> void:
--- scenes/delivery_zone.gd
+++ scenes/delivery_zone.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
 @export var item: InvItem
+@export var item2: InvItem
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		item = body.check_inv(GameFunctions.element_index)
+		item2 = body.check_inv(GameFunctions.element_index2)
 		if item:
 			var cnt_items = body.remove_item(item)
 			if cnt_items:
-				GameFunctions.update_delivery(cnt_items)
+				GameFunctions.update_delivery(cnt_items, 1)
+		if item2:
+			var cnt_items2 = body.remove_item(item2)
+			if cnt_items2:
+				GameFunctions.update_delivery(cnt_items2, 2)
